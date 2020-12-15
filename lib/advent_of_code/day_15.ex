fef3583e8add2a8491f0baf526d233cc4a807203
Theo Dowling <thdowling@gmail.com>
2020-12-15 21:52:31 +0200

    Day 15

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,7 +1,41 @@
 defmodule AdventOfCode.Day15 do
-  def part1(_args) do
+  def part1(input) do
+    begin =
+      input
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    numbers_with_positions =
+      begin
+      |> Enum.with_index(1)
+      |> Enum.into(%{})
+
+    next_number(0, Enum.count(begin), numbers_with_positions, 2019)
   end
 
-  def part2(_args) do
+  def next_number(n, l, _numbers_with_pos, count) when l == count, do: n
+
+  def next_number(n, l, numbers_with_pos, count) do
+    case Map.get(numbers_with_pos, n) do
+      nil ->
+        next_number(0, l + 1, Map.put(numbers_with_pos, n, l + 1), count)
+
+      pos ->
+        next_number(l - pos + 1, l + 1, Map.put(numbers_with_pos, n, l + 1), count)
+    end
+  end
+
+  def part2(input) do
+    begin =
+      input
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    numbers_with_positions =
+      begin
+      |> Enum.with_index(1)
+      |> Enum.into(%{})
+
+    next_number(0, Enum.count(begin), numbers_with_positions, 29_999_999)
   end
 end
--- test/advent_of_code/day_15_test.exs
+++ test/advent_of_code/day_15_test.exs
@@ -3,19 +3,28 @@ defmodule AdventOfCode.Day15Test do
 
   import AdventOfCode.Day15
 
-  @tag :skip
+  # @tag :skip
   test "part1" do
-    input = nil
+    input = "0,3,6"
     result = part1(input)
 
-    assert result
+    assert result == 436
+    assert part1("1,3,2") == 1
+    assert part1("2,1,3") == 10
+    assert part1("1,2,3") == 27
+    assert part1("2,3,1") == 78
+    assert part1("3,2,1") == 438
+    assert part1("3,1,2") == 1836
   end
 
   @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
+    assert part2("0,3,6") == 175_594
+    assert part2("1,3,2") == 2578
+    assert part2("2,1,3") == 3_544_142
+    assert part2("1,2,3") == 261_214
+    assert part2("2,3,1") == 6_895_259
+    assert part2("3,2,1") == 18
+    assert part2("3,1,2") == 362
   end
 end
