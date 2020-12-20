1034e05227c41b1f5efd7b20e1c3f70249b0f918
Theo Dowling <thdowling@gmail.com>
2020-12-20 10:55:49 +0200

    Day 18

--- lib/advent_of_code/day_18.ex
+++ lib/advent_of_code/day_18.ex
@@ -1,7 +1,45 @@
 defmodule AdventOfCode.Day18 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> File.stream!()
+    |> Enum.map(fn line -> line_result(line) end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def line_result(line) do
+    {result, []} =
+      line
+      |> String.replace("*", "-")
+      |> Code.string_to_quoted!()
+      |> replace_operators(%{:- => :*})
+      |> Code.eval_quoted()
+
+    result
+  end
+
+  defp replace_operators(op, _replace) when is_number(op), do: op
+
+  defp replace_operators({operator, metadata, [a, b]}, operators) do
+    {operators[operator] || operator, metadata,
+     [replace_operators(a, operators), replace_operators(b, operators)]}
+  end
+
+  def part2(input) do
+    input
+    |> File.stream!()
+    |> Enum.map(fn line -> advanced_line_result(line) end)
+    |> Enum.sum()
+  end
+
+  def advanced_line_result(line) do
+    {result, []} =
+      line
+      |> String.replace("*", "-")
+      |> String.replace("+", "/")
+      |> Code.string_to_quoted!()
+      |> replace_operators(%{:- => :*, :/ => :+})
+      |> Code.eval_quoted()
+
+    result
   end
 end
--- test/advent_of_code/day_18_test.exs
+++ test/advent_of_code/day_18_test.exs
@@ -3,19 +3,20 @@ defmodule AdventOfCode.Day18Test do
 
   import AdventOfCode.Day18
 
-  @tag :skip
+  # @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+    assert line_result("2 * 3 + (4 * 5)") == 26
+    assert line_result("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 437
+    assert line_result("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240
+    assert line_result("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632
   end
 
-  @tag :skip
+  # @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
+    assert advanced_line_result("1 + (2 * 3) + (4 * (5 + 6))") == 51
+    assert advanced_line_result("2 * 3 + (4 * 5)") == 46
+    assert advanced_line_result("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 1445
+    assert advanced_line_result("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 669_060
+    assert advanced_line_result("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 23340
   end
 end
